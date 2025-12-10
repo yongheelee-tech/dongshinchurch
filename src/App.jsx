80916bba8fe44e09334e5826a433e47fe23c68f0
cloudante <yonghee.lee@dongshinchurch.com>
2025-12-09 16:50:21 -0800

    Add page transition animations and update App component for route change effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 import Home from './pages/Home'
 import PastorGreeting from './pages/PastorGreeting'
 import WorshipTimes from './pages/WorshipTimes'
 import ServingPeople from './pages/ServingPeople'
 
 function App() {
+  const location = useLocation()
+  const [isAnimating, setIsAnimating] = useState(false)
+
+  useEffect(() => {
+    // Trigger animation on route change and initial load
+    setIsAnimating(false)
+    const timer = setTimeout(() => {
+      setIsAnimating(true)
+    }, 10)
+    
+    return () => clearTimeout(timer)
+  }, [location.pathname])
+
+  // Trigger animation on initial mount
+  useEffect(() => {
+    setIsAnimating(true)
+  }, [])
+
   return (
-    <div className="font-sans">
+    <div className={`font-sans ${isAnimating ? 'page-enter' : ''}`}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/pastor-greeting" element={<PastorGreeting />} />
