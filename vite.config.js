97b2b881b34259d84c908a4ab67ce42ac4f8e86a
cloudante <yonghee.lee@dongshinchurch.com>
2025-12-11 23:20:49 -0800

    Add path alias for images in Vite configuration to simplify imports

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@img': resolve(__dirname, './img'),
+    },
+  },
 })
 
